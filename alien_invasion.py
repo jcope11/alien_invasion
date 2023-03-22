99ed2ab96cf84e818f39b46f028a460cf0f9704a
Joe Copeland <jcope11@gmail.com>
2023-03-22 09:01:13 -0700

    9th commit, aliens move to the right and left and drop down the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -41,7 +41,8 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
 
     def _update_aliens(self):
-        """Update the positions of all the aliens in the fleet."""
+        """Check if the fleet is at an edge, then update the positions of all the aliens in the fleet."""
+        self._check_fleet_edges()
         self.aliens.update()
 
     def _create_fleet(self):
@@ -53,7 +54,7 @@ class AlienInvasion:
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
 
-        # Determine the number of rows of aliens that fit on the sreen
+        # Determine the number of rows of aliens that fit on the screen
         ship_height = self.ship.rect.height
         available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
         number_rows = available_space_y // (2 * alien_height)
@@ -72,14 +73,18 @@ class AlienInvasion:
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
-    # REMOVE UNUSED CODE
-    # Create the first row of aliens.
-    # self._create_alien(alien_number)
-    # def _create_alien(self, alien_number):
-    # alien_width = alien.rect.width
-        # # Make an alien.
-        # alien = Alien(self)
-        # self.aliens.add(alien)
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
